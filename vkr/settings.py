82e1ca4a3d2df406acd6ae35ffea3fa262f4d902
Ilya Alekhin <50171844+Zhichi57@users.noreply.github.com>
2022-07-21 11:18:34 +0300

    Update settings.py

--- vkr/settings.py
+++ vkr/settings.py
@@ -164,8 +164,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ilya22121997@yandex.ru'
-EMAIL_HOST_PASSWORD = 'hxdtmysjevxpwniw'
-DEFAULT_FROM_EMAIL = 'ilya22121997@yandex.ru'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = ''
 EMAIL_USE_TLS = True
 EMAIL_SUBJECT = 'Оповещение системы мониторинга микроклимата'
